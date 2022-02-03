c6af1cef877d2a12aef3faad93f215d2e271ed34
Vlad oancea <vladioanoancea@gmail.com>
2022-02-03 20:31:32 +0000

    Submitted initial empty project

---
+++ README.md
@@ -0,0 +1,3 @@
+Project Overgrown is a student game developed in Unreal Engine 4.27.
+
+This is the source code and assets.
---
+++ Source/Overgrown/OvergrownGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OvergrownGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AOvergrownGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AOvergrownGameMode();
+};
+
+
+
