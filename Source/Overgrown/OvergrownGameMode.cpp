576a8689660678403c37744a2e53d91e2c61c334
Vlad Oancea <vladioanoancea@gmail.com>
2022-04-24 17:21:32 +0100

    - started populating the scene with props; - fixed stupid error that would always pop up when opening project

--- Source/Overgrown/OvergrownGameMode.cpp
+++ Source/Overgrown/OvergrownGameMode.cpp
@@ -7,7 +7,7 @@
 AOvergrownGameMode::AOvergrownGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TemplateProject/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
